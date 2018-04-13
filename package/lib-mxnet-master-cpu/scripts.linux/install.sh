8f1e1060b687f66a49f83d9017a55cd0de38255c
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-04-13 13:10:42 +0000

    added options to compile MxNet on FireFly

--- package/lib-mxnet-master-cpu/scripts.linux/install.sh
+++ package/lib-mxnet-master-cpu/scripts.linux/install.sh
@@ -20,8 +20,19 @@ echo "Preparing vars for MxNet ..."
 # Check extra stuff
 export MXNET_LIB_DIR=${INSTALL_DIR}/lib
 
+EXTRA_MAKE_FLAGS=""
+if [ "${USE_F16C}" != "" ] ; then
+   EXTRA_MAKE_FLAGS="${EXTRA_MAKE_FLAGS} USE_F16C=${USE_F16C}"
+fi
+
 cd ${INSTALL_DIR}/src
 
-make ${CK_MAKE_BEFORE} -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} USE_OPENCV=${USE_OPENCV} USE_BLAS=${USE_BLAS}
+make ${CK_MAKE_BEFORE} -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} USE_OPENCV=${USE_OPENCV} \
+     USE_BLAS=${USE_BLAS} ${EXTRA_MAKE_FLAGS}
+
+if [ "${?}" != "0" ] ; then
+  echo "Error: installation failed!"
+  exit 1
+fi
 
 return 0
