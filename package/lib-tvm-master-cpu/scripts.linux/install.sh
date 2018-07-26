0570faa334718fb1ecbf7caf8a3d67855551b1ca
Xilinx User <xilinx@pynq>
2018-07-26 16:51:08 +0000

    fixing FPGA server

--- package/lib-tvm-master-cpu/scripts.linux/install.sh
+++ package/lib-tvm-master-cpu/scripts.linux/install.sh
@@ -25,11 +25,14 @@ rm -f tmp-pip-help.tmp
 echo "Downloading and installing deps ..."
 echo ""
 
+INSTALL_PYTHON_LIBS="decorator tornado psutil wget"
+if [ "${TVM_FPGA_SERVER}" != "ON" ] ; then
+   INSTALL_PYTHON_LIBS="${PYTHON_LIBS} xgboost nose pylint numpy nose-timer cython scipy"
+fi
+
 EXTRA_PYTHON_SITE=${INSTALL_DIR}/src/python
 
-${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --ignore-installed decorator tornado psutil xgboost wget \
-                                                                 nose pylint numpy nose-timer cython scipy \
-                                                                 -t ${EXTRA_PYTHON_SITE} ${SYS}
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --ignore-installed ${INSTALL_PYTHON_LIBS} -t ${EXTRA_PYTHON_SITE} ${SYS}
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
   exit 1
