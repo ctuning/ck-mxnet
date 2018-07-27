7170709487e16e91b4a87ea7e28f41ecedcea402
Xilinx User <xilinx@pynq>
2018-07-27 07:56:25 +0000

    fixing pynq

--- program/tvm-vta-pynq-server/start_rpc_server.sh
+++ program/tvm-vta-pynq-server/start_rpc_server.sh
@@ -7,5 +7,9 @@ echo ""
 ifconfig eth0
 echo "********************************************************************************"
 
-${CK_ENV_COMPILER_PYTHON_FILE} -m tvm.exec.rpc_server
-# --load-library ${CK_ENV_LIB_VTA_SERVER_LIB_FULL}
+#cd ${CK_ENV_LIB_TVM_SRC}
+
+PROJROOT=${CK_ENV_LIB_TVM_SRC}
+export PYTHONPATH=${PYTHONPATH}:${PROJROOT}/python:${PROJROOT}/vta/python
+
+${CK_ENV_COMPILER_PYTHON_FILE} -m vta.exec.rpc_server
